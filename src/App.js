aaa45bf945b7858bff09a6c00f68e44c8be3994d
emredemirel39 <emredemirel39@gmail.com>
2022-05-17 21:05:19 +0300

    created small project for handling forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const initialState = {
+    firstName: '',
+    surname: '',
+    age: '',
+    password: '',
+    city: ''
+  };
+  const [ form, setForm ] = useState(initialState);
+
+  const handleChange = (e) => {
+    e.preventDefault()
+
+    setForm({...form, [e.target.name]: e.target.value})
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => handleChange(e)} value={form.firstName} name='firstName' type="text" placeholder="First Name" />
+        <input onChange={(e) => handleChange(e)} value={form.surname} name='surname' type="text" placeholder="Surname" />
+        <input onChange={(e) => handleChange(e)} value={form.age} name='age' type="text" placeholder="Age" />
+        <input onChange={(e) => handleChange(e)} value={form.password} name='password' type="password" placeholder="Password" />
+        <select onChange={(e) => handleChange(e)} value={form.city} name="city">
+          <option value="">Choose your city</option>
+          <option value="istanbul">Istanbul</option>
+          <option value="krasnodar">Krasnodar</option>
+          <option value="moscow">Moscow</option>
+          <option value="izmir">Izmir</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
